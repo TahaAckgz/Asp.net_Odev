a3555ef28daaf877fd4325fd347dbdc17857fec6
Taha Açıkgöz <3700tahayavuz@gmail.com>
2024-02-27 12:11:44 +0200

    a
    a

---
+++ Asp.net/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using _net_ilk_odev.Models;
+
+namespace _net_ilk_odev.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    public IActionResult Saat()
+    {
+        return View();
+        
+        
+    }
+     public IActionResult Country()
+    {
+        string[] CountryList = {
+            "Rusya",
+            "Irak",
+            "Suudi Arabistan",
+            "Sudan",
+            "Yemen",
+            "Somali",
+           
+        };
+
+        ViewBag.Countrys = CountryList;
+
+        return View();
+    }
+
+}
---
+++ Asp.net/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _net_ilk_odev.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
